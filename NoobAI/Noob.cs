f8f393a5434d0b46ccb7a75a926d5e387f6f5474
sherlockchang <sherlockchang@outlook.com>
2016-03-16 16:09:29 +0800

    changes

---
+++ NoobAI/Noob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NoobAI
+{
+    public class Noob
+    {
+        private static Noob mInst = new Noob();
+        public static Noob Instance {
+            get
+            {
+                return mInst;
+            }
+        }
+
+        private Noob() { }
+
+        public void Init()
+        {
+            //
+        }
+    }
+}
---
+++ NoobAI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoobAI
+{
+    //out put console
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Noob.Instance.Init();
+        }
+    }
+}
